5bef9a6e3030e29d256fa4d2af7873e5e08acba5
Eduarda Valentim <dudaahvalentim@gmail.com>
2019-11-20 18:14:43 -0300

    modificando a questao 7

--- listas/lista-de-exercicio-05/questao07.py
+++ listas/lista-de-exercicio-05/questao07.py
@@ -1,5 +1,7 @@
 def valorPagamneto(valor, dias):
-	if dias > 0:
+	if dias == 0:
+		return valor
+	else:
 		multa = valor + (valor * 0.03) 
 		juros = dias * 0.001
 		valor_novo = multa + juros
@@ -13,15 +15,17 @@ print('O valor a ser pago será R${:.2f}'.format(valorPagamneto(valor_prestacao,
 relatorio.append(valorPagamneto(valor_prestacao, dias_atraso))
 
 # Loop
-while (valor_prestacao == dias_atraso) != 0:
+while valor_prestacao != 0:
 	valor_prestacao = float(input('Qual o valor da prestação? '))
 	dias_atraso = int(input('Quantos dias essa prestação está em atraso? '))
 	print('O valor a ser pago será R${:.2f}'.format(valorPagamneto(valor_prestacao, dias_atraso)))
 	relatorio.append(valorPagamneto(valor_prestacao, dias_atraso))
 
 # relatório
+print('\n')
+print('{:^15}'.format('RELATÓRIO DO DIA'))
 print('Foram pagas {} prestações'.format(len(relatorio) - 1))
-print('O valor total das prestações: {:.2f}'.format(sum(relatorio)))
+print('O valor total das prestações: R${:.2f}'.format(sum(relatorio)))
 
 """
 preciso saber a diferença entre ENCERRAR o programa e não GERAR MULTA.
