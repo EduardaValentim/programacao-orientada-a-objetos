5e8512a986aa914f95c9a9d05184c4f0d38ac5aa
Eduarda Valentim <dudaahvalentim@gmail.com>
2019-11-21 20:55:03 -0300

    colocando dentro de uma função

--- listas/lista-de-exercicio-05/questao01.py
+++ listas/lista-de-exercicio-05/questao01.py
@@ -1,4 +1,6 @@
+def enesima(n):
+    for lista in range(1, n + 1):
+    	print(('%d '%lista)*lista)
+	
 n = int(input('Digite um número: '))
-
-for lista in range(1, n + 1):
-	print(('%d '%lista)*lista)
+enesima(n)
