ed6f06f919083dfc0ce8734d9a998cbc14759bcf
Eduarda Valentim <dudaahvalentim@gmail.com>
2019-11-21 21:09:38 -0300

    adicionando função

--- listas/lista-de-exercicio-05/questao08.py
+++ listas/lista-de-exercicio-05/questao08.py
@@ -1,2 +1,5 @@
-numero = str(input('Digite um número inteiro: '))
-print('Esse número contém {} dígitos'.format(len(numero)))
+def contar_digitos(num):
+	soma = len(num)
+	return soma
+num = str(input('Digite um número inteiro: ')).strip()
+print('Esse número contém {} dígitos'.format(contar_digitos(num)))
