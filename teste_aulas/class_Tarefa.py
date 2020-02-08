2be5369bca17269c978e99555e2aaddff0bd4dea
Eduarda Valentim <dudaahvalentim@gmail.com>
2020-02-08 11:09:47 -0300

    atualizando repositório

--- teste_aulas/class_Tarefa.py
+++ teste_aulas/class_Tarefa.py
@@ -1,4 +1,25 @@
 from datetime import datetime
+
+class Projeto:
+    def __init__(self, nome):
+        self.nome = nome
+        self.tarefas = []
+
+    def adicionar(self, descricao):
+        self.tarefas.append(Tarefa(descricao))
+
+    def procurar(self, descricao):
+        for tarefa in self.tarefas:
+            if tarefa.descricao == descricao:
+                return tarefa
+    
+    def pendentes(self):
+        tarefas_pendentes = []
+        for tarefa in self.tarefas:
+            if tarefa.feito == False:
+                tarefas_pendentes.append(tarefa)
+        return tarefas_pendentes
+            
 class Tarefa:
     def __init__(self, descricao):
         self.descricao = descricao
@@ -9,14 +30,38 @@ class Tarefa:
         self.feito = True
     
     def __str__(self):
-        imprimir = 'Data:' + str(self.criacao) + '\nDescrição:' + self.descricao + '\nStatus' + str(self.feito)
+        imprimir = 'Data: ' + str(self.criacao) + '\nDescricao: ' + self.descricao + '\nStatus: ' + str(self.feito)
         return imprimir
 
 def main():
+    '''
     casa = []
     t1 = Tarefa('Lavar os pratos.')
-    t2 = Tarefa('Lavar os pratos.')
-    print(t1)
+    t2 = Tarefa('Lavar roupa.')
+    t3 = Tarefa('Escovar os dentes.')
+
+    casa.append(t1)
+    casa.append(t2)
+    casa.append(t3)
+
+    for tarefa in casa:
+        if tarefa.descricao == 'Lavar roupa.':
+            tarefa.concluir()
+    
+    for tarefa in casa:
+        print(tarefa)
+        print('\n')
+    '''
+    casa = Projeto('Tarefas de casa')
+
+    casa.adicionar('Lavar os pratos.')
+    casa.adicionar('Lavar roupa.')
+    casa.adicionar('Escovar os dentes.')
+
+    casa.procurar('Escovar os dentes.').concluir()
+    for tarefa in casa.pendentes():
+        print(tarefa)
+        print('\n')
 
 if __name__ =='__main__':
     main()
