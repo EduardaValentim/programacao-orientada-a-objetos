e632452a84e8ef8ccbbab9eb486a419955e42428
Eduarda Valentim <dudaahvalentim@gmail.com>
2020-02-11 10:59:36 -0300

    adicionando a pasta Provas, e a prova da N1 etapa 1 juntamente com seus arquivos extras

---
+++ provas/N1-1/prova.py
@@ -0,0 +1,24 @@
+def alterar_bytes(numero):
+    resultado = numero / 1000000
+    return resultado
+def porcentagem(mega):
+    resultado = 100 * mega / 3528.30
+    return resultado
+
+soma = 0
+numero_usuario = 1
+with open('usuarios.txt') as dados:
+    with open('relatorio.txt', 'w') as escrever:
+        print('ACME Inc.        Uso do espaço em disco pelos usuários', file=escrever)
+        print('--'*28, file=escrever)
+        print('{:<3}   {:<10}   {:<17}     {:<9}'.format('Nr.', 'Usuário', 'Espaço utilizado', '% do uso'), file=escrever)
+        print(' ', file=escrever)
+        for linha in dados:
+            linha_quebrada = linha.strip().split(' ')
+            mega = alterar_bytes(int(linha_quebrada[1]))
+            print('{:<3}   {:<10}    {:>10.2f} MB      {:>9.2f}%'.format(numero_usuario, linha_quebrada[0], mega, porcentagem(mega)),file=escrever)
+            numero_usuario += 1
+            soma += mega
+        print(' ', file=escrever)
+        print('Espaço total ocupado: {:.2f} MB'.format(soma), file=escrever)  
+        print('Espaço médio ocupado: {:.2f} MB'.format(soma/numero_usuario), file=escrever)   
